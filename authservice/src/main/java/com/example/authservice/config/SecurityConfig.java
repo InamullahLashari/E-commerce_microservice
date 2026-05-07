e18cf055f877b9ca23e186bc99362c22ef0d55cc
inam lashari <lashariinam50@gmail.com>
2026-05-07 04:55:07 -0700

    add notification service

--- authservice/src/main/java/com/example/authservice/config/SecurityConfig.java
+++ authservice/src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
