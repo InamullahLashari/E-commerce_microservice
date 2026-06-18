a239dd82930ae4754c58af6f458da098f8580b65
inam lashari <lashariinam50@gmail.com>
2026-06-18 04:38:31 -0700

    add openConfig

---
+++ apigateway/src/main/java/com/bitsandbytes/apigateway/config/OpenAPIConfig.java
@@ -0,0 +1,58 @@
+package com.bitsandbytes.apigateway.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("${server.port:8080}")
+    private int serverPort;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Gateway")
+                        .version("1.0.0")
+                        .description("""
+                                API Gateway for Microservices with:
+                                - JWT Authentication
+                                - Rate Limiting
+                                - Circuit Breaker
+                                - Request/Response Logging
+                                - Metrics & Monitoring
+                                """)
+                        .contact(new Contact()
+                                .name("API Gateway Team")
+                                .email("support@example.com")
+                                .url("https://github.com/example/api-gateway"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:" + serverPort)
+                                .description("Local Development Server")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .schemaRequirement("Bearer Authentication", new SecurityScheme()
+                        .name("Authorization")
+                        .type(Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(In.HEADER)
+                        .description("Enter JWT token: Bearer <token>"));
+    }
+}
