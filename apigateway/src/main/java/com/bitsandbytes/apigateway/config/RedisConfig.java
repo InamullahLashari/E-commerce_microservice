9db7ca7914a29d251efb6588a125d3d29d2a6d73
inam lashari <lashariinam50@gmail.com>
2026-06-18 04:26:38 -0700

    update RedisConfig

--- apigateway/src/main/java/com/bitsandbytes/apigateway/config/RedisConfig.java
+++ apigateway/src/main/java/com/bitsandbytes/apigateway/config/RedisConfig.java
@@ -1,18 +1,46 @@
 package com.bitsandbytes.apigateway.config;
 
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Mono;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public KeyResolver ipKeyResolver(){
-        return exchange -> {
-            String ip = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
-            return Mono.just(ip);
-        };
+    @Primary
+    public ReactiveRedisTemplate<String, String> reactiveRedisStringTemplate(
+            ReactiveRedisConnectionFactory connectionFactory) {
+
+        RedisSerializationContext<String, String> serializationContext = RedisSerializationContext
+                .<String, String>newSerializationContext(new StringRedisSerializer())
+                .key(new StringRedisSerializer())
+                .value(new StringRedisSerializer())
+                .hashKey(new StringRedisSerializer())
+                .hashValue(new StringRedisSerializer())
+                .build();
+
+        return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
+    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, Object> reactiveRedisObjectTemplate(
+            ReactiveRedisConnectionFactory connectionFactory) {
+
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        RedisSerializationContext<String, Object> serializationContext = RedisSerializationContext
+                .<String, Object>newSerializationContext(new StringRedisSerializer())
+                .key(new StringRedisSerializer())
+                .value(serializer)
+                .hashKey(new StringRedisSerializer())
+                .hashValue(serializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
     }
 }
